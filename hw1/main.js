e7d52b425eb88be481daadd7e4f9a88609397204
volodya92dutchuk <dutchuk.volodya@gmail.com>
2022-11-09 22:26:31 +0200

    initial commit

---
+++ hw1/main.js
@@ -0,0 +1,43 @@
+let foo = 'hello';
+console.log(foo);
+document.write(foo);
+
+foo = 'owu';
+console.log(foo)
+
+foo = 'com';
+console.log(foo);
+
+let ret = 'ua';
+console.log(ret);
+alert(ret);
+
+let num = '1';
+console.log(num);
+
+const PI = 3.14;
+console.log(PI);
+
+let ag = 2 > 3;
+console.log(ag);
+
+ag = 300 > 299;
+console.log(ag);
+
+let fn = 'Volodymyr';
+let mn = 'Vasylovych';
+let ln = 'Dutchuk';
+console.log(`${fn} ${mn} ${ln}`);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof true);
+
+
+
+
---
+++ hw2/main.js
@@ -0,0 +1,52 @@
+let arr = [13, 7, 'probe', false, true,];
+console.log(arr)
+let user = {
+    title: 'harryPotter',
+    pageCount: 700,
+    genre: 'fantasy'
+}
+console.log(user)
+
+let user1 = {
+    tittle: 'Game of Thrones',
+    pageCount: 1300,
+    genre: 'Serial drama',
+    authors: {
+        name: 'G R Martin',
+        age: 75
+    }
+
+}
+console.log(user1)
+console.log(user1['authors'])
+
+
+let user11 = {name: 'Piter', username: 'Pier', password: 789};
+let user2 = {name: 'Kolya', username: 'Kil', password: 456};
+let user3 = {name: 'Ivan', username: 'Io', password: 123};
+let user4 = {name: 'Ella', username: 'Ellis', password: 987};
+let user5 = {name: 'Mary', username: 'Mariya', password: 654};
+
+let party = [user11,user2,user3,user4,user5]
+console.log(party)
+console.log(party[0].password)
+console.log(party[1].password)
+console.log(party[2].password)
+console.log(party[3].password)
+console.log(party[4].password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
